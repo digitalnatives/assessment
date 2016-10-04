cdd6888f772f3572329351b286844de12b1771ac
Benkő Gábor <Benk Gbor>
2016-10-04 12:39:59 +0200

    code cleanup for formatter

--- js-numerals/src/format.js
+++ js-numerals/src/format.js
@@ -3,6 +3,14 @@ var simples = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', '
 var tenses = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
 function convertNumberToText(number, delimiter = '') {
+    function calculateNextNumber(currentNumber, substracter) {
+        return currentNumber - substracter;
+    }
+
+    function setResultFromSimples(simplesindex, decimalName) {
+        return simples[simplesindex] + ' '+ decimalName;
+    }
+
     var number = number.replace(/\s/g, '');
     var result = '';
     if(isNaN(number)) {
@@ -21,19 +29,20 @@ function convertNumberToText(number, delimiter = '') {
     } else if (numberInt >= 20 && numberInt <= 99) {
         var tenseNumber = Math.trunc(numberInt / 10);
         var tenseText = tenses[tenseNumber - 2];
-        nextNumber = numberInt - (tenseNumber * 10);
-        return result += delimiter + tenseText + convertNumberToText(String(nextNumber) , '-');
+        nextNumber = calculateNextNumber(numberInt, tenseNumber * 10);
+        result = delimiter + tenseText + convertNumberToText(String(nextNumber) , '-');
     } else if (numberInt >= 100 && numberInt <= 1999) {
         var hundred = Math.trunc(numberInt / 100);
-        nextNumber = numberInt - (hundred * 100);
-        return result += simples[hundred - 1] + ' hundred' + convertNumberToText(String(nextNumber), ' and ');
+        nextNumber = calculateNextNumber(numberInt, hundred * 100);
+        result = setResultFromSimples(hundred - 1, 'hundred') + convertNumberToText(String(nextNumber), ' and ');
     } else if (numberInt === 2000) {
         return 'two thousand';
     } else if (numberInt >= 2001 && numberInt <= 99999) {
         var thousand = Math.trunc(numberInt / 1000);
-        nextNumber = numberInt - (thousand * 1000);
-        return result += simples[thousand - 1] + ' thousand' + convertNumberToText(String(nextNumber), ' and ');
+        nextNumber = calculateNextNumber(numberInt, thousand * 1000);
+        result = setResultFromSimples(thousand - 1, 'thousand') + convertNumberToText(String(nextNumber), ' and ');
+    } else {
+        return 'Error: Your given number is not supported to translate. Please give a number between 1 - 99999';
     }
-
-    return 'Error: Your given number is not supported to translate. Please give a number between 1 - 99999';
+    return result;
 }
