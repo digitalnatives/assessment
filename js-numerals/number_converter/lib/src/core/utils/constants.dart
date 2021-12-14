b5c530d815c6b47bcf97371849f23e39896f0d07
tath.gergely <tath.gergely@gmail.com>
2021-12-14 10:56:18 +0100

    number constants added

--- js-numerals/number_converter/lib/src/core/utils/constants.dart
+++ js-numerals/number_converter/lib/src/core/utils/constants.dart
@@ -1,1 +1,46 @@
+
+//App config 
 const String kAppTitle = 'Number Converter';
+
+//Singular
+const String kZero = 'zero'; //0
+const String kOne = 'one'; //1
+const String kTwo = 'two'; //2
+const String kThree ='three'; //3
+const String kFour = 'four'; //4
+const String kFive = 'five'; //5
+const String kSix = 'six'; //6
+const String kSeven ='seven'; //7
+const String kEight = 'eight'; //8
+const String kNine = 'nine'; //9
+
+//Tens
+const String kTen = 'ten'; //10
+const String kTwenty = 'twenty'; //20
+const String kThirty = 'thirty'; //30
+const String kFourty = 'fourty'; //40
+const String kFifty = 'fifty'; //50
+const String kSixty = 'sixty'; //60
+const String kSeventy = 'seventy'; //70
+const String kEighty = 'eighty'; //80
+const String kNinety = 'ninety'; //90
+
+//Unique
+const String kEleven = 'eleven'; //11
+const String kTwelve = 'twelve'; //12
+const String kThirteen = 'thirteen'; //13
+const String kFourteen ='fourteen'; //14
+const String kFifteen = 'fifteen'; //15
+const String kSixteen = 'sixteen'; //16
+const String kSeventeen = 'seventeen'; //17
+const String kEighteen = 'eighteen'; //18
+const String kNineteen = 'nineteen'; //19
+
+const String kHundred = 'hundred'; //100
+const String kThousand = 'thousand'; //1000
+const String kMillion = 'million'; //1000 000
+const String kBillion = 'billion'; //1000 000 000
+const String kTrillion = 'trillion'; //1000 000 000
+
+//Other
+const String kNumberConnector = '-';
