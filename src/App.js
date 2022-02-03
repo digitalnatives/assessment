2d089dab9630bbd4792f3e17afaf67aede769e18
Papp Bence <papp.bence15@gmail.com>
2022-02-03 16:33:06 +0100

    feat: Allow user to give a number, display the result.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import convertToArabic from "./util/numberConvertion";
+import {useState} from "react";
+
 function App() {
+    const [inputValue, setInputValue] = useState("");
+
   return (
-    "Hello world!"
+    <div>
+      <div>
+        <h2 id="output">{convertToArabic(inputValue)}</h2>
+      </div>
+      <div>
+        <label htmlFor="input">Number:</label>
+        <input type="number" min="0" id="input" value={inputValue} onChange={handleChange}/>
+      </div>
+    </div>
   );
+
+  function handleChange(event) {
+      setInputValue(event.target.value)
+  }
 }
 
 export default App;
