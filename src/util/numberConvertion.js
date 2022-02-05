1fd2a942261e56d1838c6836cf89365334f36642
Papp Bence <papp.bence15@gmail.com>
2022-02-05 16:32:02 +0100

    refactor: Generate output according to english grammar rules.

--- src/util/numberConvertion.js
+++ src/util/numberConvertion.js
@@ -1,32 +1,47 @@
-export default function convertToArabic(number){
-    return createOutput(number);
-}
-
-function createOutput(number){
-    const numberLen = number.length;
-
-    switch (numberLen){
-        case 1:
-            return zeroToNine[number];
-        case 2:
-            return twoDigits(number, false);
-        case 3:
-            const lastTwoDigits = twoDigits(number.slice(-2), true);
-            return `${zeroToNine[number[0]]} ${hundreds[0]} ${lastTwoDigits}`;
-        case 4:
-            return `${zeroToNine[number[0]]} ${hundreds[1]} ${getHundred(number.slice(-3))} ${twoDigits(number.slice(-2), true)}`;
-        case 5:
-            return `${twoDigits(number.slice(0, 2), false)} ${hundreds[1]} ${getHundred(number.slice(-3))} ${twoDigits(number.slice(-2), true)}`
-        case 6:
-            return `${zeroToNine[number[0]]} ${hundreds[0]} ${twoDigits(number.slice(1, 3), true)} ${hundreds[1]} ${getHundred(number.slice(-3))} ${twoDigits(number.slice(-2), true)}`
-        case 7:
-            return `${zeroToNine[number[0]]} ${hundreds[2]} ${zeroToNine[number[1]]} ${hundreds[0]} ${twoDigits(number.slice(2, 4), true)} ${hundreds[1]} ${getHundred(number.slice(-3))} ${twoDigits(number.slice(-2), true)}`
-        default:
-            return "Type in a number!"
+export default function convertToArabic(input){
+    if(validateInput(input)){
+        return createOutput(formatInput(input));
+    }else{
+        return "Type in a number!";
     }
 }
 
-function twoDigits(twoDigits, needAnd) {
+function validateInput(input){
+    return input !== ""
+}
+
+function formatInput(input){
+    return parseInt(input);
+}
+
+function createOutput(input){
+    const number = input.toString()
+    const numberLen = number.length;
+    const finalTwoDigit = numberLen > 2 ? twoDigits(number.slice(-2)) : "";
+    switch (numberLen){
+        case 1:
+            if(parseInt(number) === 0){
+                return "zero"
+            }
+            return zeroToNine[number];
+        case 2:
+            return twoDigits(number);
+        case 3:
+            return `${getHundred(number[0], finalTwoDigit, true)} ${finalTwoDigit}`;
+        case 4:
+            return createString(`${getThousand(number[0])} ${getHundred(number.slice(-3), finalTwoDigit, true)} ${finalTwoDigit}`);
+        case 5:
+            return createString(`${getThousand(number.slice(0, 2))} ${getHundred(number.slice(-3), finalTwoDigit, true)} ${finalTwoDigit}`);
+        case 6:
+            return createString(`${getHundredThousand(number[0], number.slice(1, 3))} ${getHundred(number[3], finalTwoDigit, true)} ${finalTwoDigit}`);
+        case 7:
+            return createString(`${zeroToNine[number[0]]} ${hundreds[2]} ${getHundredThousand(number[1], number.slice(2, 4))} ${getHundred(number[4], finalTwoDigit, true)} ${finalTwoDigit}`);
+        default:
+            return "Higher numbers are coming soon!";
+    }
+}
+
+function twoDigits(twoDigits) {
     let result;
     if(parseInt(twoDigits) === 0){
         return ""
@@ -34,15 +49,62 @@ function twoDigits(twoDigits, needAnd) {
         result = zeroToNine[twoDigits[1]];
     } else if (parseInt(twoDigits) >= 20) {
         const secondNumber = zeroToNine[twoDigits[1]] === "" ? "" : "-" + zeroToNine[twoDigits[1]]
-        result = tensMultiple[twoDigits[0]] + secondNumber
+        result = tenMultiples[twoDigits[0]] + secondNumber
     } else {
         result = tenToTwenty[twoDigits];
     }
-    return needAnd ? "and " + result : result;
+    return result;
 }
 
-function getHundred(hundred){
-    return zeroToNine[hundred[0]] === "" ? "" : zeroToNine[hundred[0]] + " " + hundreds[0]
+function getHundred(hundred, nextNumber, lastHundred){
+    let result = zeroToNine[hundred[0]] === "" ? "" : zeroToNine[hundred[0]];
+    if(lastHundred){
+        if(nextNumber !== "" && result === ""){
+            result += "and";
+        }else if(nextNumber === ""){
+            return result + " " + hundreds[0];
+        }else{
+            result += " " + hundreds[0] + " and"
+        }
+    }
+    return result;
+}
+
+function getThousand(thousand){
+    let result = "";
+    if(parseInt(thousand) < 10){
+        result += zeroToNine[thousand];
+    }else{
+        result += twoDigits(thousand);
+    }
+    return result + " " + hundreds[1];
+}
+
+function getHundredThousand(hundred, thousand){
+    let result = ""
+    const thousandPart = twoDigits(thousand);
+    const hundredPart = getHundred(hundred, thousandPart, false);
+    if(hundredPart === ""){
+        return thousandPart + " " + hundreds[1];
+    }else if(thousandPart !== ""){
+        result += hundredPart + " " + hundreds[0] + " and " + thousandPart + " " + hundreds[1]
+    }else{
+        result += hundredPart + " " + hundreds[0] + " " + hundreds[1];
+    }
+    return result;
+}
+
+function createString(convertedNumber){
+    const splitConvertedNumbers = convertedNumber.split(" ").filter(element => element !== "");
+    let formattedString = "";
+    for (let i = 0; i < splitConvertedNumbers.length; i++) {
+        if(i === splitConvertedNumbers.length - 1){
+            formattedString += splitConvertedNumbers[i];
+        }else{
+            formattedString += splitConvertedNumbers[i] + " ";
+        }
+    }
+    return formattedString;
 }
 
 const zeroToNine = {
@@ -71,7 +133,7 @@ const tenToTwenty = {
     "19": "nineteen",
 }
 
-const tensMultiple = {
+const tenMultiples = {
     "2": "twenty",
     "3": "thirty",
     "4": "forty",
