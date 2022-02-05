771a89d11496d81d0e56a64591e67aa9cffbae3f
Sjpeti97 <sjpeti97@gmail.com>
2022-02-05 15:20:37 +0100

    feat: add the Header and NumToChar components to App

--- arabic-number-converter/src/App.js
+++ arabic-number-converter/src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header";
+import NumToChar from "./components/NumToChar";
 
 function App() {
   return (
     <div className="App">
-
+        <Header />
+        <div className={"content"}>
+            <h1>Type a number here to check how to write it!</h1>
+            <NumToChar />
+        </div>
     </div>
   );
 }
