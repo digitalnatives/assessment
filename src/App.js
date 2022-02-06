88dbf0970c5e43b3855a27b3ccc7288ee52fa60e
Papp Bence <papp.bence15@gmail.com>
2022-02-06 22:31:52 +0100

    feat: Position UI elements in the middle of the page.

--- src/App.js
+++ src/App.js
@@ -11,27 +11,30 @@ function App() {
 
   return (
       <Container maxWidth={"sm"}>
-          <Grid container spacing={4}>
-              <Grid item>
-                  <img src="https://i.imgur.com/E4Q5eZe.png" alt="logo" width={100}/>
-              </Grid>
-          </Grid>
       <Grid container
-            spacing={0}
+            spacing={2}
             direction="column"
             alignItems="center"
             justifyContent="center"
-            style={{ minHeight: '30vh' }}>
+            style={{ minHeight: '60vh' }}>
+
+          <Grid item>
+              <img src="https://i.imgur.com/E4Q5eZe.png" alt="logo" width={100}/>
+          </Grid>
+
           <Grid item xs={3}>
               <div className="converted-input-display-container">
                   <h3 id="output">{convertToText(inputValue)}</h3>
               </div>
+          </Grid>
+
+          <Grid item>
               <div className="input-container">
                   <input type="number" min="0" id="input" value={inputValue} onChange={handleChange}/>
               </div>
           </Grid>
-      </Grid>
-          <Grid container spacing={4} alignItems="center">
+
+          <Grid container rowSpacing={5} columnSpacing={1} alignItems="center" justifyContent={"center"} maxWidth={300} marginTop={5}>
               {numKeyboard.map((key)=>(
                   <Grid item xs={4} key={key} textAlign={"center"}>
                       <div className="num-key">
@@ -40,6 +43,8 @@ function App() {
                   </Grid>
               ))}
           </Grid>
+
+      </Grid>
       </Container>
   );
 
@@ -50,6 +55,8 @@ function App() {
   function handleClick(key){
       if(key === "backspace"){
           setInputValue(inputValue.slice(0, -1))
+      }else if(key === "i"){
+          window.open("https://github.com/pappbence15")
       }else{
           setInputValue(inputValue + key)
       }
