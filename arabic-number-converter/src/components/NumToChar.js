4d7ac88bcaa008deb64a7fa5bd0306ba6e28fbfe
Sjpeti97 <sjpeti97@gmail.com>
2022-02-07 00:55:48 +0100

    feat: add support for hundreds (1-999)

--- arabic-number-converter/src/components/NumToChar.js
+++ arabic-number-converter/src/components/NumToChar.js
@@ -27,20 +27,28 @@ function convertToString(number) {
     const ones = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
     const tens = ["",["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"],
         "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
-    const collector = [ones, tens];
+    const hundred = " hundred and "
+    const collector = [ones, tens, ones];
     let numLength = (number.length);
+    const numberAsString = number.toString();
     if (numLength >= 1) {
-        if (number>9 && number<20) {
-            let digit = number.toString()[0]
-            numString = collector[numLength-1][digit][number.toString()[number.toString().length-1]]
-        }
-        else {
             for (let i = 0; i < numLength; i++) {
-                let digit = parseInt(number.toString()[i]);
+                let digit = parseInt(numberAsString[i]);
+                let place = numLength-(i+1);
                 if (digit !== 0) {
-                    numString += collector[numLength-(i+1)][digit]
+                    if (place === 1 && digit === 1) {
+                        numString += collector[place][digit][numberAsString[numLength-1]];
+                        break;
+                    }
+                    numString += collector[place][digit]
+                    if (numLength-i === 2 && parseInt(numberAsString[numLength-1])!==0) {
+                        numString+="-"
+                    }
+                    if (place===2) {
+                        numString += hundred
+                    }
                 }
-            }
+            // }
         }
     }
     return numString
