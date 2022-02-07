998f4c07fb63230b41f0a8cb7202e4773b529905
Papp Bence <papp.bence15@gmail.com>
2022-02-07 17:17:47 +0100

    feat: Add info icon.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import convertToText from "./util/numberConversion";
 import {useState} from "react";
 import {Container, Grid} from "@mui/material"
 import BackspaceIcon from '@mui/icons-material/Backspace';
+import PersonSearchIcon from '@mui/icons-material/PersonSearch';
 import "./style/app.css"
 
 function App() {
     const [inputValue, setInputValue] = useState("");
     const backspaceIcon = <BackspaceIcon/>;
+    const infoIcon = <PersonSearchIcon/>;
     const numKeyboard = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "i", "0", "backspace"];
 
   return (
       <Container maxWidth={"sm"}>
+          <div className={"main-container"}>
       <Grid container
-            spacing={2}
+            spacing={1}
             direction="column"
             alignItems="center"
             justifyContent="center"
@@ -24,27 +27,34 @@ function App() {
 
           <Grid item xs={3}>
               <div className="converted-input-display-container">
-                  <h3 id="output">{convertToText(inputValue)}</h3>
+                  <h2 id="output">{convertToText(inputValue)}</h2>
               </div>
           </Grid>
 
           <Grid item>
               <div className="input-container">
-                  <input type="number" min="0" id="input" value={inputValue} onChange={handleChange}/>
+                  <input className="input" type="number" min="0" id="input" value={inputValue} onChange={handleChange}/>
               </div>
           </Grid>
 
-          <Grid container rowSpacing={5} columnSpacing={1} alignItems="center" justifyContent={"center"} maxWidth={300} marginTop={5}>
+          <Grid
+              container
+              rowSpacing={1}
+              columnSpacing={2}
+              maxWidth={300}
+              marginTop={1}
+              alignItems="center">
               {numKeyboard.map((key)=>(
-                  <Grid item xs={4} key={key} textAlign={"center"}>
-                      <div className="num-key">
-                          <a href="#" onClick={()=>handleClick(key)}>{key === "backspace"? backspaceIcon : key}</a>
+                  <Grid item xs={4} key={key}>
+                      <div className="num-key-container" onClick={()=>handleClick(key)}>
+                          <h2>{key === "i" ? infoIcon : key === "backspace"? backspaceIcon : key}</h2>
                       </div>
                   </Grid>
               ))}
           </Grid>
 
       </Grid>
+          </div>
       </Container>
   );
 
