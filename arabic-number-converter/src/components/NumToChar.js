bc82d96dabdf4b176574d8c6d40fe46ea2c64eab
Sjpeti97 <sjpeti97@gmail.com>
2022-02-08 16:12:56 +0100

    refactor/feat: refactor the logic and add support up to 9billion(99999999). Because of the reactor found an easy way to extend the logic.

--- arabic-number-converter/src/components/NumToChar.js
+++ arabic-number-converter/src/components/NumToChar.js
@@ -1,9 +1,9 @@
-import {useEffect, useState} from "react";
+import {useState} from "react";
 
 export default function NumToChar() {
-    const [number, setNumber] = useState('');
+    const [number, setNumber] = useState(0);
 
-    let stringNum = convertToString(number);
+    let numSentence = convertToSentence(number);
 
     return(
         <div>
@@ -13,44 +13,75 @@ export default function NumToChar() {
                 min={1}
                 step={1}
             />
-            <h2>{stringNum}</h2>
+            <h2>{numSentence}</h2>
         </div>
     )
 }
 
-function convertToString(number) {
-    let numString = "";
-    const ones = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-    const tens = ["",["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"],
-        "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
-    const hundred = " hundred "
-    const and = "and "
-    const collector = [ones, tens, ones];
-    let numLength = (number.length);
+function convertToSentence(number) {
+    let sentence = "";  //   [0[0-10], 1[0-10], 2[0-7]] indexes
+    const collection = [ //[ [0-9], [10-19], 20-90] nums
+        ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"],
+        ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"],
+        ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+    ];
+    const and = "and ";
+    const milestones = [" hundred ", " thousand ", " million ", " billion "];
+    const numLength = number.length;
     const numberAsString = number.toString();
+    let placeValue = 0;
+
+    const onePlaces = [0, 2, 3, 5, 7, 8, 9, 11];
+    const teenPlaces = [1, 4, 6, 10];
+
     if (numLength >= 1) {
         for (let i = 0; i < numLength; i++) {
-            let digit = parseInt(numberAsString[i]);
-            let place = numLength-(i+1);
+            const digit = parseInt(numberAsString[i]);
+            const reversedPlace = numLength-(i+1);
+            const nextDigit = parseInt(numberAsString[i+1]);
+
             if (digit !== 0) {
-                if (place === 1 && digit === 1) {
-                    numString += collector[place][digit][numberAsString[numLength-1]];
-                    break;
+
+                if (teenPlaces.includes(reversedPlace)) {
+                    if (digit === 1) {
+                        placeValue = 1;
+                        sentence += collection[placeValue][nextDigit];
+                    } else {
+                        placeValue = 2;
+                        sentence += collection[placeValue][digit];
+                    }
+                    if (parseInt(numberAsString[i+1]) !== 0) {
+                        sentence += "-";
+                    }
                 }
-                numString += collector[place][digit]
-                if (numLength-i === 2 && parseInt(numberAsString[numLength-1])!==0) {
-                    numString+="-"
-                }
-                if (place===2) {
-                    numString += hundred;
-                    if (parseInt(numberAsString[numLength-1]) !== 0 || parseInt(numberAsString[numLength-2]) !== 0) {
-                        numString += and;
+
+                if (onePlaces.includes(reversedPlace)) {
+                    placeValue = 0;
+                    sentence += collection[placeValue][digit];
+                    switch (reversedPlace){
+                        case 2:
+                            sentence += milestones[0];
+                            if (nextDigit !== 0 || parseInt(numberAsString[i+2]) !== 0) {
+                                sentence += and;
+                            }
+                            break;
+                        case 3:
+                            sentence += milestones[1];
+                            if (nextDigit === 0  && (parseInt(numberAsString[i+2]) !== 0 || parseInt(numberAsString[i+3]) !== 0)) {
+                                sentence += and;
+                            }
+                            break;
+                        case 5:
+                            sentence += milestones[2];
+                            break;
+                        case 7:
+                            sentence += milestones[3];
                     }
                 }
             } else if (numLength === 1) {
-                numString = 0;
+                sentence = 0;
             }
         }
     }
-    return numString
+    return sentence;
 }
