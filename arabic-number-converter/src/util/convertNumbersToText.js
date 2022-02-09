e6102233c4aec1d363639e1e6a867bb3c898fe57
Sjpeti97 <sjpeti97@gmail.com>
2022-02-09 14:33:30 +0100

    refactor: extract parts of the code to separate functions

--- arabic-number-converter/src/util/convertNumbersToText.js
+++ arabic-number-converter/src/util/convertNumbersToText.js
@@ -11,15 +11,17 @@ const onePlaces = [0, 2, 3, 5, 6, 8, 9, 11];
 const teenPlaces = [1, 4, 7, 10];
 const hundredPlaces = [2, 5, 8, 11];
 const milestonePlaces = [3, 6, 9];
+let placeValue = 0;
+let sentence;
+let skipNextDigit;
 
 
 export default function convertToSentence(number) {
-    let sentence = "";
+    sentence = "";
+    skipNextDigit = false;
+
     const numLength = number.length;
     const numberAsString = number.toString();
-    let placeValue = 0;
-
-    let skipNextDigit = false;
 
     if (numLength >= 1) {
         for (let i = 0; i < numLength; i++) {
@@ -27,51 +29,79 @@ export default function convertToSentence(number) {
             const reversedPlace = numLength - (i + 1);
             const nextDigit = parseInt(numberAsString[i + 1]);
 
-            if (teenPlaces.includes(reversedPlace)) {
-                if (digit === 1) {
-                    placeValue = 1;
-                    sentence += collection[placeValue][nextDigit];
-                    skipNextDigit = true;
-                } else {
-                    placeValue = 2;
-                    sentence += collection[placeValue][digit];
-                    if (nextDigit !== 0 && digit !== 0) {
-                        sentence += "-";
-                    }
-                }
+            if (onePlaces.includes(reversedPlace) && (digit !== 0 || milestonePlaces.includes(reversedPlace))) {
+                dealWithOnes(digit, nextDigit, numberAsString, reversedPlace, i);
             }
 
-            if (digit !== 0 || milestonePlaces.includes(reversedPlace)) {
-                if (onePlaces.includes(reversedPlace)) {
-                    const digitAfterNext = parseInt(numberAsString[i + 2]);
-                    const previousDigit = parseInt(numberAsString[i - 1]);
-                    placeValue = 0;
-                    if (!skipNextDigit) {
-                        sentence += collection[placeValue][digit];
-                    } else {
-                        skipNextDigit = false;
-                    }
-                    switch (true) {
-                        case hundredPlaces.includes(reversedPlace):
-                            sentence += " hundred ";
-                            if (nextDigit !== 0 || digitAfterNext !== 0) {
-                                sentence += and;
-                            }
-                            break;
-                        case milestonePlaces.includes(reversedPlace):
-                            if (digit !==0 || previousDigit !== 0 || parseInt(numberAsString[i - 2]) !== 0) {
-                                sentence += milestones[reversedPlace];
-                            }
-                            if (reversedPlace===3) {
-                                if (nextDigit === 0 && (digitAfterNext !== 0 || parseInt(numberAsString[i + 3]) !== 0)) {
-                                    sentence += and;
-                                }
-                            }
-                            break;
-                    }
-                }
+            if (teenPlaces.includes(reversedPlace)) {
+                dealWithTeens(digit, nextDigit);
             }
         }
     }
     return sentence;
+}
+
+function dealWithOnes(digit, nextDigit, numberAsString, reversedPlace, i) {
+    const digitAfterNext = parseInt(numberAsString[i + 2]);
+
+    placeValue = 0;
+
+    addDigitIfNecessary(digit);
+
+    switch (true) {
+        case hundredPlaces.includes(reversedPlace):
+            addHundredPlusAndWord(nextDigit, digitAfterNext);
+            break;
+        case milestonePlaces.includes(reversedPlace):
+            addMilestonesPlusAndWord(digit, nextDigit, digitAfterNext, numberAsString, reversedPlace, i);
+            break;
+    }
+}
+
+function addDigitIfNecessary(digit) {
+    if (!skipNextDigit) {
+        sentence += collection[placeValue][digit];
+    } else {
+        skipNextDigit = false;
+    }
+}
+
+function addHundredPlusAndWord(nextDigit, digitAfterNext) {
+    sentence += " hundred ";
+    if (nextDigit !== 0 || digitAfterNext !== 0) {
+        sentence += and;
+    }
+}
+
+function addMilestonesPlusAndWord(digit, nextDigit, digitAfterNext, numberAsString, reversedPlace, i) {
+    if (digit !==0 || parseInt(numberAsString[i - 1]) !== 0 || parseInt(numberAsString[i - 2]) !== 0) {
+        sentence += milestones[reversedPlace];
+    }
+    if (reversedPlace===3) {
+        if (nextDigit === 0 && (digitAfterNext !== 0 || parseInt(numberAsString[i + 3]) !== 0)) {
+            sentence += and;
+        }
+    }
+}
+
+function dealWithTeens(digit, nextDigit) {
+    if (digit === 1) {
+        addTeensThatStartsWith1ToSentence(nextDigit);
+    } else {
+        addTeensToSentence(digit, nextDigit);
+    }
+}
+
+function addTeensThatStartsWith1ToSentence(nextDigit) {
+    placeValue = 1;
+    sentence += collection[placeValue][nextDigit];
+    skipNextDigit = true;
+}
+
+function addTeensToSentence(digit, nextDigit) {
+    placeValue = 2;
+    sentence += collection[placeValue][digit];
+    if (nextDigit !== 0 && digit !== 0) {
+        sentence += "-";
+    }
 }
