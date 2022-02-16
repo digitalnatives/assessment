6b17f416380301c5271cd4b2fb93cd461c839f57
iamrobertw <69533417+iamrobertw@users.noreply.github.com>
2022-02-16 21:49:47 +0100

    style: making the code more readable

--- js-numerals/script.js
+++ js-numerals/script.js
@@ -2,50 +2,85 @@
 
 //~ Made by Robert Więckowski
 
-let num = "zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen".split(" ");
-let tens = "twenty thirty forty fifty sixty seventy eighty ninety".split(" ");
+let numbers =
+  'zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen'.split(
+    ' ',
+  );
+let tens = 'twenty thirty forty fifty sixty seventy eighty ninety'.split(' ');
 
-function n2w(n){
-    let ct = (~~((n-(~~(~~(n/1000000))*1000000))/1000)) == 0 ? '': n2w(~~((n-(~~(~~(n/1000000))*1000000))/1000)) + " thousand";
-  
-    if (!n) return "Enter a number in the input field"
-    if (n < 20) return num[n];
-    let dig = n%10;
-    if (n < 100) return tens[~~(n/10)-2] + (dig? "-" + num[dig]: "");
-    if (n < 1000) return num[~~(n/100)] +" hundred" + (n%100 == 0? "": " and " + n2w(n%100));
-    if (n >= 1000 && n < 10000 && (n - n%100) === ~~(n/1000)*1000) return n2w(~~(n/1000)) + " thousand" + (n%1000 != 0? " and " + n2w(n%1000): "");
-    if (n >= 1000 && n < 1000000) return n2w(~~(n/1000)) + " thousand" + (n%1000 != 0? " " + n2w(n%1000): "");
-    if (n >= 1000000 && n < 1000000000) return n2w(~~(~~(n/1000)/1000)) + ' million ' + ct  + (n%1000 != 0? " " + n2w(n%1000): "");
-    if (n >= 1000000000 && n <= 999999999999) return n2w(~~(~~(~~(n/1000)/1000)/1000)) + ' billion ' + ((n%1000000 != 0) ? n2w((~~(~~(n/1000)/1000))%1000)  + ' million ' : "") + ct  + (n%1000 != 0? " " + n2w(n%1000): "");
-    if (n > 999999999999) return "This number is outside the range of the converter"
-    return     
-    }
+console.log(~~1.5);
 
-//* And using this scheme, it is quite easy to add further code and convert larger numbers 
+function numberToWords(n) {
+  let cutThousand =
+    ~~((n - ~~~~(n / 1000000) * 1000000) / 1000) == 0
+      ? ''
+      : numberToWords(~~((n - ~~~~(n / 1000000) * 1000000) / 1000)) + ' thousand';
+
+  if (!n) return 'Enter a number in the input field';
+  if (n < 20) return numbers[n];
+  let digits = n % 10;
+  if (n < 100) return tens[~~(n / 10) - 2] + (digits ? '-' + numbers[digits] : '');
+  if (n < 1000)
+    return (
+      numbers[~~(n / 100)] +
+      ' hundred' +
+      (n % 100 == 0 ? '' : ' and ' + numberToWords(n % 100))
+    );
+  if (n >= 1000 && n < 10000 && n - (n % 100) === ~~(n / 1000) * 1000)
+    return (
+      numberToWords(~~(n / 1000)) +
+      ' thousand' +
+      (n % 1000 != 0 ? ' and ' + numberToWords(n % 1000) : '')
+    );
+  if (n >= 1000 && n < 1000000)
+    return (
+      numberToWords(~~(n / 1000)) +
+      ' thousand' +
+      (n % 1000 != 0 ? ' ' + numberToWords(n % 1000) : '')
+    );
+  if (n >= 1000000 && n < 1000000000)
+    return (
+      numberToWords(~~(~~(n / 1000) / 1000)) +
+      ' million ' +
+      cutThousand +
+      (n % 1000 != 0 ? ' ' + numberToWords(n % 1000) : '')
+    );
+  if (n >= 1000000000 && n <= 999999999999)
+    return (
+      numberToWords(~~(~~(~~(n / 1000) / 1000) / 1000)) +
+      ' billion ' +
+      (n % 1000000 != 0
+        ? numberToWords(~~(~~(n / 1000) / 1000) % 1000) + ' million '
+        : '') +
+      cutThousand +
+      (n % 1000 != 0 ? ' ' + numberToWords(n % 1000) : '')
+    );
+  if (n > 999999999999)
+    return 'This number is outside the range of the converter';
+  return;
+}
+
+//* And using this scheme, it is quite easy to add further code and convert larger numbers
 
 // Text Display
 
 function displayText() {
-    let inputVal = input.value;
-    let handle = n2w(inputVal);
-    output.innerHTML = handle;
+  let inputVal = input.value;
+  let handle = numberToWords(inputVal);
+  output.innerHTML = handle;
 }
 let button = document.getElementById('changeText');
 let output = document.getElementById('output');
 let input = document.getElementById('input');
 input.addEventListener('input', displayText);
 
-// Characters Validation 
+// Characters Validation
 
-let inputField = document.getElementById("input");
+let inputField = document.getElementById('input');
 
-let invChars = [
-  "-",
-  "+",
-  "e",
-];
+let invChars = ['-', '+', 'e'];
 
-inputField.addEventListener("keydown", function(e) {
+inputField.addEventListener('keydown', function (e) {
   if (invChars.includes(e.key)) {
     e.preventDefault();
   }
@@ -53,15 +88,15 @@ inputField.addEventListener("keydown", function(e) {
 
 //* Note of Tasks and some console.logs
 
-// console.log(n2w(7))
-// console.log(n2w(42))
-// console.log(n2w(1999))
-// console.log(n2w(2001))
-// console.log(n2w(17999))
-// console.log(n2w(342251))
-// console.log(n2w(1300420))
-// console.log(n2w(81300420))
-// console.log(n2w(55241300420))
+// console.log(numberToWords(7))
+// console.log(numberToWords(42))
+// console.log(numberToWords(1999))
+// console.log(numberToWords(2001))
+// console.log(numberToWords(17999))
+// console.log(numberToWords(342251))
+// console.log(numberToWords(1300420))
+// console.log(numberToWords(81300420))
+// console.log(numberToWords(55241300420))
 
 /*
 7       === seven
@@ -72,4 +107,3 @@ inputField.addEventListener("keydown", function(e) {
 342251  === three hundred and forty-two thousand two hundred and fifty-one
 1300420 === one million three hundred thousand four hundred and twenty
 */
-
