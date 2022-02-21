594eb9fc927989df672cbc517defa2db06e07a3b
Sjpeti97 <sjpeti97@gmail.com>
2022-02-21 16:11:29 +0100

    feat: add a todo module for handling CRUD operations for JSON file with the necessary basic methods

---
+++ node-todo/models/todos.model.js
@@ -0,0 +1,56 @@
+//todo file kezelés
+
+// functions load, add find, update
+
+const fs = require('fs/promises');
+
+let filePath = '';
+
+const setFilePath = (path) => {
+    filePath = path;
+}
+
+const checkFilePath = () => {
+    if (!filePath) {
+        throw 'Filepath is missing'
+    }
+}
+
+const saveTodos = (todos) => {
+
+}
+
+const loadTodos = async () => {
+
+}
+
+const findTodo = async (id) => {
+
+}
+
+const deleteTodo = (id) => {
+    //filter
+    //save to json
+}
+
+const createTodo = (todo) => {
+    //save to json
+}
+
+const updateTodo = async (id, todo) => {
+    //map + ellenőrzés a colback elején
+    //object spreading
+    //save to json
+}
+
+module.exports = {
+    setFilePath,
+    saveTodos,
+    loadTodos,
+    findTodo,
+    deleteTodo,
+    createTodo,
+    updateTodo
+}
+
+
