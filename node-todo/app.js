670e1b3c8875fe68903da63e2ed4aa983c8a76f8
Sjpeti97 <sjpeti97@gmail.com>
2022-02-21 16:04:29 +0100

    feat: set up the basic parts of the application

--- node-todo/app.js
+++ node-todo/app.js
@@ -1,6 +1,18 @@
+const path = require('path');
+
 const express = require('express');
+const todosApi = require('./routes/todos.routes')
+
+const {setFilePath} = require("./models/todos.model");
+
+setFilePath(path.join(__dirname, 'todos.json'))
 
 const app = express();
 const port = 3000;
 
+app.use(express.json());//req.body contains the http body
+app.use('/todos',todosApi);
+
 app.listen(port);
+
+
