2ca259594d09feef3be94c981dc265b19ae9b394
Ölei <olei.viktoria@gmail.com>
2022-03-01 22:45:48 +0100

    change name convention

--- js-numerals/test.js
+++ js-numerals/test.js
@@ -1,32 +1,28 @@
 import converter from "./script";
 
 test("Conversion test, ", () => {
-  const seven = converter("7");
-  expect(seven).toBe("seven");
+  const singleDigit = converter("7");
+  expect(singleDigit).toBe("seven");
 
-  const fortyTwo = converter("42");
-  expect(fortyTwo).toBe("forty-two");
+  const doubleDigits = converter("42");
+  expect(doubleDigits).toBe("forty-two");
 
-  const oneThousandNineHundred = converter("1999");
-  expect(oneThousandNineHundred).toBe(
-    "one thousand nine hundred and ninety-nine"
-  );
+  const threeDigits = converter("1999");
+  expect(threeDigits).toBe("one thousand nine hundred and ninety-nine");
 
-  const twoThousandAndOne = converter("2001");
-  expect(twoThousandAndOne).toBe("two thousand and one");
+  const fourDigits = converter("2001");
+  expect(fourDigits).toBe("two thousand and one");
 
-  const seventeenThousandNineHundred = converter("17999");
-  expect(seventeenThousandNineHundred).toBe(
-    "seventeen thousand nine hundred and ninety-nine"
-  );
+  const fiveDigits = converter("17999");
+  expect(fiveDigits).toBe("seventeen thousand nine hundred and ninety-nine");
 
-  const threeHundredAndFortytwoThousand = converter("342251");
-  expect(threeHundredAndFortytwoThousand).toBe(
+  const sixDigits = converter("342251");
+  expect(sixDigits).toBe(
     "three hundred and forty-two thousand two hundred and fifty-one"
   );
 
-  const oneMillionThreeHundredThousand = converter("1300420");
-  expect(oneMillionThreeHundredThousand).toBe(
+  const sevenDigits = converter("1300420");
+  expect(sevenDigits).toBe(
     "one million three hundred thousand four hundred and twenty"
   );
 });
